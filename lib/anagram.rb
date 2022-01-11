6f52415443448ffd886853fea5a2376466ca5d05
Jerry Tong <zihangtong@gmail.com>
2022-01-11 12:36:22 -0500

    commit tree d0deddeb902ef327804a7e6b6fc52144db209087

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(words)
+        words.filter do |w|
+            if w.chars.sort == @word.chars.sort
+                w
+            end
+        end
+    end
+
+end
